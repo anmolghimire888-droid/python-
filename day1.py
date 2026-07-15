bf3e152aad76d63acd13273d1e1adc4a90dc3624
anmol ghimire <anmol ghimire888gmail.com>
2026-07-15 15:39:40 +0545

    day1 file

--- day1.py
+++ day1.py
@@ -1,1 +1,67 @@
-print("hello world")
+#----Arthamatical operator---
+# a = 5 
+# b = 10
+
+# example fo operater in arthamatic operator 
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b) # normal division 
+# print(a//b) # not normal division bucs. it ignore the value of flote (quotient, int division)
+# print(a%b) # modulus
+# print(a**b) # Square--
+
+#Relation Operator  
+
+# It gives value in [true or false ]
+
+# print(5>6) # grater than
+# print(5>3) # less than 
+# print(5>=2) # grater tha or equal to 
+# print(4>=7) # less tha or equal too
+# print(4==4) # means equal to
+# print(4!=3) # means not equals toop
+
+#logical operator [and , or , not ]
+
+# for , and operator
+# print(5>2 and 3>1 and 5>8) # if all codition is true then only it's true either it is false
+
+# for , or operator
+# print(4<6 or 5>4)  # -> if any one condition is true it display true either it is false 
+
+# for not operator
+# print(not 5<6) # simpaly it make condition opposit ?
+
+# eng_mask = int(input("Enter your english mask: "))
+# eng_passmask = 30
+# eng_passed = eng_passmask <= eng_mask
+
+# print(eng_passed)
+
+# if ans else elif are called comtrol flow 
+# : --> this is called colon
+
+# english_mask = 30
+# input("Entre you mask in engish:")
+# print("pass" if english_mask>=30 else "fail") ----Ternari operator yo towright if..else in shortcut
+
+# passing_mask = 35
+# english_mask = int(input("Enter your english mask: "))
+
+# if 90<english_mask <=100:
+#     print("A+")
+# elif 90>english_mask>80  :
+#     print("A")
+# elif 80>english_mask >70:
+#     print("B+")
+# elif 70> english_mask>50 :
+#     print ("B")
+# elif 50>english_mask >30 :
+#     print("c+")
+# elif 35 >english_mask>0 :
+#     print("Better luck next time !")
+# else : 
+#     print("not valid")
+
