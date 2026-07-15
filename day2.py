4531f8c094b2d544a20b9635afce8a1c71240711
anmol ghimire <anmol ghimire888gmail.com>
2026-07-15 15:51:15 +0545

    day 2 n 3

---
+++ day2.py
@@ -0,0 +1,9 @@
+# ------RULE FOR VARIABLE IN PYTHON-------
+
+# ---starting leeter must be a letter or underscore 
+# ---variable must a single number ,if you want to write more than it you can write underscore
+# ---variable must not be an resurve keyword otherwise it will make an error in progyaram
+
+# english_mask = 30
+# input("Entre you mask in engish:")
+# print("pass" if engl
---
+++ day3.py
@@ -0,0 +1,59 @@
+# --------- EXIRCISE 1 --------------------
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second numbere: "))
+
+# addition = num1 + num2
+# subtraction = num1 - num2
+# multiplication = num1 * num2
+# division = num1 / num2
+
+# print("addition", addition)
+# print("subtraction", subtraction)
+# print("multipolication", multiplication)
+# print("division" , division)
+
+
+# ----------- EXIRCISE 2 ---------------
+
+# number = int(input("Enter a number to find even or odd: "))
+
+# if number % 2 == 0:
+#     print("The number you provided is even")
+# else :
+#     print("The number you provided is odd")
+
+# ---------- EXIRCISE 3 ----------------
+
+# age = int(input("Enter your age to determine you are aligable to vote or not: "))
+
+# if 0 < age < 18 :
+#     print("You are not aligable to vote")
+# elif 18 < age < 100 :
+#     print("Cong, You are aligable to vote")
+# else :
+#     print(" AGG ERROR")
+
+# ---------- EXIRCISE 4 ----------------
+
+# num = int(input("Enter the number to chake it's positive or negative: "))
+
+# if num > 0 :
+#     print("The number you provided is positve")
+# elif num < 0 :
+#     print("The number you provided is Negatiive")
+# else :
+#     print(" ZERO ")
+
+# ---------- EXIRCISE 5 ---------------------
+
+# num1 = float(input("Enter your first number: "))
+# num2 = float(input("Enter your second number: "))
+
+# if num1 > num2 :
+#     print(f"{num1} is gratter than {num2}")
+# elif num1 < num2 :
+#     print(f"{num2}is gratter than {num1}")
+# else :
+#     print("----------Both number are equal------------")
+
