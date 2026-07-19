ad96327f29c07fb7b80a7adf55514908169d874d
anmol ghimire <anmol ghimire888gmail.com>
2026-07-19 14:49:58 +0545

    halfof day4

--- day4.py
+++ day4.py
@@ -35,19 +35,55 @@
 # for num in range(10 , 0 , -1):
 #     print(f"{number} X {num} = {number*num}")
 
-sentence = "We love nepal" #-> string is an sequence of a character 
-vovals = "aeiouAEIOU"
-count = 0
+# sentence = "We love nepal" #-> string is an sequence of a character 
+# vovals = "aeiouAEIOU"
+# count = 0
 # char in vovals:
 #  'W' in "aeiouAEIOU" => False 
 #   "e" in "aeiouAEIOU" => True 6
-for char in sentence :
-    # check if it's vowel 
-    if  char in vovals:
-        print(char)  
-        count += 1
-print ("Num of vowel in sentence", count )
+# for each_char in sentence :
+    # check if it's vowel  
+#     if  each_char in vovals:
+#         print(each_char)
+#         count += 1
+# print("Num of vowecls in sentence is", count )
 
+# ----------- INFINITE LOOP -----------
+#  ----WHILE------------
+ 
+# line_xa  = True # YESS
 
+# fan loop
+# while line_xa :
+#     print("fam ghumdaixa")
+     
+#     user_answer = input("Line xa ?? (yes/no):")
+    
+#     "no"= "no" =>True
+#     if user_answer == "no" :
+#          line_xa = False
 
+# Account block new variyant 
 
+dummey_password = "adimin123"
+MAX_ATTEMPTS = 3
+user_attempt = 0
+
+while user_attempt < MAX_ATTEMPTS :
+    user_password = input("Enter your password: ")
+
+    if user_password != dummey_password :
+        print("Wrong password")
+        user_attempt += 1
+        print("Attemes left", MAX_ATTEMPTS-user_attempt)
+    else :
+        print("Login Successfull")
+        print("End PROGRAM")
+        break
+else:
+    print("You have attemped wrong passwoed for all attempes")
+    print("So your account has been temorarily blocked. Visited office for re-acctivation in nearest branch ")
+
+# --------NOTE-
+# --continue => skip the code below 
+# --break => means sstrikly dont run code below 
